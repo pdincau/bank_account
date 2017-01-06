ff0b04d31b81ac8bc07e3888e4637cca8f70f597
Paolo D'Incau <paolo.dincau@gmail.com>
2017-01-06 11:29:29 +0100

    use events and implement deposit

--- lib/bank_account.ex
+++ lib/bank_account.ex
@@ -1,19 +1,34 @@
 defmodule BankAccount do
 
   def start do
-    loop
+    loop([])
   end
 
-  defp loop do
+  defp loop(events) do
     receive do
       {:check_balance, pid} ->
-        divulge_balance(pid)
+        divulge_balance(pid, events)
+        loop(events)
+      {:deposit, amount} ->
+        events = deposit(amount, events)
+        loop(events)
     end
-    loop
   end
 
-  defp divulge_balance(pid) do
-    send(pid, {:balance, 0})    
+  defp divulge_balance(pid, events) do
+    send(pid, {:balance, calculate_balance(events)})    
   end
 
+  defp calculate_balance(events) do
+    deposits = sum(events)
+    deposits
+  end
+
+  defp sum(events) do
+    Enum.reduce(events, 0, fn({_, amount}, acc) -> acc + amount end)
+  end
+
+  defp deposit(amount, events) do
+    events ++ [{:deposit, amount}]
+  end
 end
--- test/bank_account_test.exs
+++ test/bank_account_test.exs
@@ -8,8 +8,17 @@ defmodule BankAccountTest do
     verify_balance_is 0, account
   end
 
+  test "has balance incremented by the amount of a deposit" do
+    account = spawn_link(BankAccount, :start, [])
+
+    send(account, {:deposit, 100})
+
+    verify_balance_is 100, account
+  end
+
   defp verify_balance_is(expected_amount, account) do
     send(account, {:check_balance, self})
     assert_receive {:balance, ^expected_amount}
   end
+
 end
